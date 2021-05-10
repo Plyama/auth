cbbfb8f9abb852d88d51e9b7da8208d7fbe83873
ynaumenko <ynaumenko@mirantis.com>
2021-05-10 15:14:47 +0300

    Added app package

---
+++ internal/app/app.go
@@ -0,0 +1,18 @@
+package app
+
+import (
+	"github.com/plyama/auth/internal/repository"
+	"gorm.io/gorm"
+)
+
+type App struct {
+	Repositories *repository.Repositories
+}
+
+func NewApp(db *gorm.DB) *App {
+	return &App{
+		Repositories: &repository.Repositories{
+			Users: repository.NewUsersRepository(db),
+		},
+	}
+}
